54808f742a0a8ba7a1a2721e1228c92ca1ecca17
jujucasseb <jucasseb@gmail.com>
2022-04-02 16:44:40 -0300

    comment edit

--- src/models/player.model.js
+++ src/models/player.model.js
@@ -17,7 +17,7 @@ function model(sequelize) {
     };
     
     return sequelize.define('Players', attributes, options);
-    //player.hasOne(Team);
+    //player.belongsTo(Team);
 }
 
 module.exports = model;
